20e458debd159960cbbadab65a6274237a74a950
pawnlord <scruffduff321@gmail.com>
2021-04-17 00:27:30 -0400

    Add sending files from client side

--- kdvxs/src/server.c
+++ kdvxs/src/server.c
@@ -24,6 +24,9 @@ void* main_loop_thread(void *vargp);
 /* parse a recieved command */
 int get_command(char* in, char*** out);
 
+/* get only the command name from in */
+int get_simple_command(char* in, char** out);
+
 /* use command once found */
 int use_command(char** comm, int size, int sock, char** retmsgp);
 
@@ -35,7 +38,6 @@ int start_server(int port){
 	int opt = 1;
 	int addrlen = sizeof(address);
 	char buffer[1024] = {0};
-	char *hello = "Hello from server";
 	text = malloc(LOG_SIZE);
 	for(int i = 0; i < LOG_SIZE; i++){
 		text[i] = 0;
@@ -135,16 +137,65 @@ int use_command(char** comm, int size, int sock, char** retmsgp) {
 		FILE* fp = fopen(comm[1], "r");
 		char* buff = malloc(1024);
 
-		fread(buff, 1, 1024, fp);
-		perror("");
-		send(sock, buff, strlen(buff), 0);
+		/* Get size of file */
+		fseek(fp, 0, SEEK_END);
+		int fsize = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
 
-		free(buff);
-		int success = check_success(sock);
-		if(success == ERR_RECIEVED){
-			strcpy(retmsg, "Oops, something went wrong. Our code monkeys are hard at work to fix it");
+		int currp = 0, currerrs = 0, success = SUCCESSFUL;
+
+		while(currp < fsize+1024){
+			if(success == SUCCESSFUL) {
+				int end = fread(buff, 1, 1024, fp);
+				buff[end] = 0;
+			}
+			send(sock, buff, strlen(buff), 0);
+			success = check_success(sock);
+			if(success == ERR_RECIEVED){
+				currerrs += 1;
+				if(currerrs > 20) break;
+			} else {
+				currp += 1024;
+				currerrs = 0;
+			}
 		}
-		return success;
+		if(currerrs > 20){
+			printf("AAAAA\n");
+			strcpy(retmsg, "20 Failed attempts, aborting");
+		}
+		return (currerrs > 20)? ERR_RECIEVED:SUCCESSFUL;
+	} else if(strcmp(comm[0], "send") == 0) {
+		if(size < 2){
+			strcpy(retmsg, "err 6: Not Enough Arguments Provided");
+			return ERR_RECIEVED;
+		}
+		char* filename = comm[1];
+		/*if( access(filename, F_OK) == 0){
+			strcpy(retmsg, "err 8: File already Exists");
+			return ERR_RECIEVED;
+		}*/
+		printf("%s", filename);
+		FILE* fp = fopen(filename, "w");
+		char *newcomm = malloc(255), *buffer = malloc(1024);
+		strcpy(buffer, "succ 2:");
+		send(sock, buffer, strlen(buffer), 0);
+		while(1){
+			int valread = read(sock, buffer, 1024);
+			buffer[valread] = 0;
+
+			get_simple_command(buffer, &newcomm);
+			if(strcmp(newcomm, "dnes") == 0) break;
+			fputs(buffer, fp);
+
+			printf("%d, %s\n", valread, buffer);
+
+			strcpy(buffer, "succ 2:");
+			send(sock, buffer, strlen(buffer), 0);
+		}
+		free(newcomm);
+		free(buffer);
+		fclose(fp);
+		return SUCCESSFUL;
 	}
 	strcpy(retmsg, "err 7: Not a command");
 	return ERR_RECIEVED;
@@ -186,7 +237,7 @@ int get_command(char* in, char*** out){
 			out_word++;
 			out_char = 0;
 			(*out)[out_word] = malloc(255);
-			for(int j = 0; j < 255; j++){
+ 			for(int j = 0; j < 255; j++){
 				(*out)[out_word][j] = 0;
 			}
 		} else{
@@ -195,6 +246,24 @@ int get_command(char* in, char*** out){
 		}
 	}
 	return out_word+1;
+}
+
+int get_simple_command(char* in, char** out){
+	if(strchr(in, ':') == NULL){
+		strcpy(*out, "");
+		return 0;
+	}
+	for(int i = 0; in[i] != 0; i++){
+		if(in[i] == ' ' || in[i] == ':'){
+			int temp = in[i];
+			in[i] = 0;
+			strcpy(*out, in);
+			printf("found: %s\n", *out);
+			in[i] = temp;
+			return 1;
+		}
+	}
+	return 0;
 }
 
 void print_command(char** out, int argc){
